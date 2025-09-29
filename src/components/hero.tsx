fd0f079eb6bf673cded6cdc6e68799cbbb25b5f6
Maobugichi <immansamuel846@gmail.com>
2025-09-29 18:56:26 +0100

    fix:clear hero ts error

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import heroBackground from '@/assets/up.png';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
